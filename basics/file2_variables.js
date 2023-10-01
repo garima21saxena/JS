6a2384fd82d2e8c3a9ad94e7cb7f14b12c5438bd
garima21saxena <141829221+garima21saxena@users.noreply.github.com>
2023-10-01 14:54:12 +0000

    variables config done

---
+++ basics/file2_variables.js
@@ -0,0 +1,13 @@
+const accountId=144
+// accountId=200 //Not allowed
+console.log(accountId);
+let email="abc@yahoo.com"
+email="pqr@gmail.com"
+console.log(email);
+var pswrd="123"
+pswrd="456"
+console.log(pswrd)
+accountCity="Jaipur"
+accountCity="Agra"
+console.log(accountCity)
+console.table([accountId,accountCity,email,pswrd])
