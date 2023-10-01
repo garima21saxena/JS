34ab644bd26f254e32c0599a9d0fde944d0f86a6
garima21saxena <141829221+garima21saxena@users.noreply.github.com>
2023-10-01 14:54:31 +0000

    datatypes config done

---
+++ basics/file3_datatypes.js
@@ -0,0 +1,15 @@
+let myName="Garima"
+let age=18
+let isloggedIn=false
+
+// Primitive datatypes
+// number=> 2to the power 52
+// bigint
+//string=""
+// boolean=>true/false
+// null=>standalone value
+// undefined =>
+// symbol=>uniqe
+
+// object
+console.log(typeof(undefined));  //undefined
