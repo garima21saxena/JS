ef03d9d22475bfa4c4b2e663049a00d2ffc2e7e2
garima21saxena <141829221+garima21saxena@users.noreply.github.com>
2023-10-02 07:09:34 +0000

    operation conversion

---
+++ basics/file5_basicmaths.js
@@ -0,0 +1,46 @@
+// let value=4
+// let negvalue=-value
+// console.log(negvalue);
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1="Hello"
+// let str2=" garima"
+// let str3=str1+str2
+// console.log(str3)
+
+// Complex to understand
+// let str1="1"
+// let str2=2
+// console.log(str1+str2)
+// OR
+// let str1=1
+// let str2="2"
+// console.log(str1+str2)
+// OR
+// let str1="1"
+// let str2="2"
+// console.log(str1+str2)
+// OR
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1=num2=num3=2+2 //Not recommended
+// console.log(num1,num2,num3);
+
+// console.log(Infinity+2)
+
+// let a=22
+// const b=++a
+// console.log(`a:${a}, b:${b}`)
+// let x=0
+// let y=x++
+// console.log(`x:${x}, y:${y}`)
