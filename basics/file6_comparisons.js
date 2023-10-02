19f1fa5fe6cb47f86333fc74893ba80b187553fd
garima21saxena <141829221+garima21saxena@users.noreply.github.com>
2023-10-02 07:27:03 +0000

    comparisons

---
+++ basics/file6_comparisons.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2"<1); //try same datatype
+
+// console.log(null==0);
+// console.log(null<=0);
+// console.log(null>=0);
+// console.log(null>0);
+// console.log(null<0);
+
+// console.log(undefined==0);
+// console.log(undefined<=0);
+// console.log(undefined>=0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// Strict check === checks datatype along with values
+console.log("2"===2); // false
